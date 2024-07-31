63e31cccd90b7fee6bac7268eb8a1bd341603921
18thday <day18th@yandex.ru>
2024-07-31 13:47:28 +0500

    Fix: ostream

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <string>
 #include <iostream>
+#include <memory>
+#include <string>
 
 class Text {
 public:
-    virtual void render(const std::string& data) const {
-        std::cout << data;
+    virtual void render(std::ostream& os, const std::string& data) const {
+        os << data;
     }
 };
 
@@ -17,72 +18,77 @@ public:
 class Paragraph : public DecoratedText {
 public:
     explicit Paragraph(Text* text) : DecoratedText(text) {}
-    void render(const std::string& data) const override {
-        std::cout << "<p>";
-        text_->render(data);
-        std::cout << "</p>";
+    void render(std::ostream& os, const std::string& data) const override {
+        os << "<p>";
+        text_->render(os, data);
+        os << "</p>";
     }
 };
 
 class Reversed : public DecoratedText {
 public:
     explicit Reversed(Text* text) : DecoratedText(text) {}
-    void render(const std::string& data) const override {
-        text_->render(std::string(data.rbegin(), data.rend()));
+    void render(std::ostream& os, const std::string& data) const override {
+        text_->render(os,std::string(data.rbegin(), data.rend()));
     }
 };
 
 class Link : public DecoratedText {
 public:
     explicit Link(Text* text) : DecoratedText(text) {}
-    void render(const std::string& link, const std::string& text) const {
-        std::cout << "<a href=";
-        text_->render(link);
-        std::cout << ">";
-        text_->render(text);
-        std::cout << "</a>";
+    void render(std::ostream& os, const std::string& link, const std::string& text) const {
+        os << "<a href=";
+        text_->render(os,link);
+        os << ">";
+        text_->render(os,text);
+        os << "</a>";
     }
 };
 
 class ItalicText : public DecoratedText {
 public:
     explicit ItalicText(Text* text) : DecoratedText(text) {}
-    void render(const std::string& data) const override {
-        std::cout << "<i>";
-        text_->render(data);
-        std::cout << "</i>";
+    void render(std::ostream& os, const std::string& data) const override {
+        os << "<i>";
+        text_->render(os, data);
+        os << "</i>";
     }
 };
 
 class BoldText : public DecoratedText {
 public:
     explicit BoldText(Text* text) : DecoratedText(text) {}
-    void render(const std::string& data) const override {
-        std::cout << "<b>";
-        text_->render(data);
-        std::cout << "</b>";
+    void render(std::ostream& os, const std::string& data) const override {
+        os << "<b>";
+        text_->render(os, data);
+        os << "</b>";
     }
 };
 
 int main() {
     {
-        auto text_block = new ItalicText(new BoldText(new Text()));
-        text_block->render("Hello world");
+        auto text = Text();
+        auto bt = BoldText(&text);
+        auto text_block = std::make_unique<ItalicText>(&bt);
+        text_block->render(std::cout, "Hello world");
         std::cout << std::endl;
     }
     {
-        auto text_block = new Paragraph(new Text());
-        text_block->render("Hello world");
+        auto text = Text();
+        auto text_block = std::make_unique<Paragraph>(&text);
+        text_block->render(std::cout,"Hello world");
         std::cout << std::endl;
     }
     {
-        auto text_block = new Reversed(new Text());
-        text_block->render("Hello world");
+        auto text = Text();
+        auto text_block = std::make_unique<Reversed>(&text);
+        text_block->render(std::cout,"Hello world");
         std::cout << std::endl;
     }
     {
-        auto text_block = new Link(new Text());
-        text_block->render("netology.ru","Hello world");
+        auto text = Text();
+        auto text_block = std::make_unique<Link>(&text);
+        text_block->render(std::cout,"netology.ru","Hello world");
         std::cout << std::endl;
     }
 }
